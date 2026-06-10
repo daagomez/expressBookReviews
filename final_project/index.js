18c03ca196e0bee91ad75e14c256342e325748de
daagomez <daagomez>
2026-06-09 21:48:43 -0500

    Task for1 to 6 done

--- final_project/index.js
+++ final_project/index.js
@@ -6,20 +6,20 @@ const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}));
+
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
-
 app.use("/customer/auth/*", function auth(req,res,next){
+    // res.status(200).json(`autorización: ${req.session.authorisation}`)
     // valid access token
-    if (req.session.authorisation) {
+    if (req.session.authorization) {
         let token = req.session.authorization['accessToken'];
-
         // Verify JWT token
         jwt.verify(token, "access", (err, user) => {
             if (!err) {
                 req.user = user;
-                next;
+                next();
             } else {
                 return res.status(403).json({message: "User not authenticated"});
             }
@@ -34,4 +34,4 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running on port: " + PORT));
